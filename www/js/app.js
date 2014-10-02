82c1c96cb5decbed2e342b08a23a29c268c1c6a8
Atul Athawale <atul_athawale@fulcrumww.com>
2014-10-02 15:37:25 +0530

    Revert "Modified www folder by Sachin"
    This reverts commit bbab25a79dfe504f0a1c2fd6121552c4a4cfbe99.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-      navigator.splashscreen.hide();
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
